f826d46f18c63b2c62dd244d1bed0370964e5935
Radhika <radhika.b4329@gmail.com>
2023-08-03 19:11:00 +0530

    docdb

--- main.tf
+++ main.tf
@@ -43,6 +43,8 @@ resource "aws_docdb_cluster" "docdb" {
   skip_final_snapshot     = true
   db_subnet_group_name    = aws_docdb_subnet_group.default.name
   vpc_security_group_ids = [aws_security_group.docdb.id]
+  storage_encrypted      = true
+  kms_key_id = ""
 
   tags = merge(
     local.common_tags,
